b421e75fe5ab0cedd05d1a6669531a8ed2a48ff0
zirtaebea <zirtaeb.dev@gmail.com>
2023-02-11 15:36:29 -0300

    Alteração

--- Ciclos_iteracoes.js
+++ Ciclos_iteracoes.js
@@ -2,5 +2,5 @@ let numero = parseInt(prompt('Insira um número de 1 a 10'))
 for (let i = 0; i<=10; i++){
     
     let multiplica = numero*i
-    alert(numero + ' x ' + i + ' = ' + 'multiplica')
+    alert(numero + ' x ' + i + ' = ' + multiplica)
 }
