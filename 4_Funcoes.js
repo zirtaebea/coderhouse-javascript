982fe2b21202fe0abcf3ab84f65fbc15c7b8ac3c
zirtaebea <zirtaeb.dev@gmail.com>
2023-02-14 21:11:16 -0300

    Alterações

---
+++ 4_Funcoes.js
@@ -0,0 +1,10 @@
+function soma(a, b){
+    alert(a + b)
+}
+
+
+//const soma = (a,b) => {a+b}
+// let e const sao mais utilizados do que o var
+//return c - retorna o valor para caso precisar utilizar
+
+soma (5, 7)
