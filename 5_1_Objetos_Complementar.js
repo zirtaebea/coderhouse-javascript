3524fb3bbe88511b4b01cbfcb2fe89403229e20b
Beatriz Gomes <112278018+zirtaebea@users.noreply.github.com>
2023-02-23 14:02:35 -0300

    NÃO TERMINADO

---
+++ 5_1_Objetos_Complementar.js
@@ -0,0 +1,29 @@
+//DEFINIR SE O NUMERO É PRIMO
+
+let numero
+
+function insereNumero(num){
+    let num = prompt('Digite um número inteiro')
+    return num
+}
+
+
+function verificaPrimo(){
+
+    let divisores = 0
+    for(let i = 1 ; i <=numero ; i++)
+        if(numero % i == 0)
+        divisores++;
+   
+        if(divisores==2){}
+      alert("O número")
+         else
+       resposta.innerHTML='Não é primo';
+ }
+ 
+}
+
+
+function proximosPrimos(){
+    for (i = 1; i > 5)
+}
