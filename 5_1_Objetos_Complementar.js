676cc010db6c2ea09177793eb87d09d1e5ff734a
zirtaebea <zirtaeb.dev@gmail.com>
2023-02-27 16:50:31 -0300

    Meses

--- 5_1_Objetos_Complementar.js
+++ 5_1_Objetos_Complementar.js
@@ -1,103 +1,57 @@
-//DEFINIR SE O NUMERO É PRIMO
-
-
-/* let num = prompt('Digite um número inteiro')
-
-function insereNumero(){
-    let numero = parseInt(num)
-    return numero
-
-}
-
-
-
-function verificaPrimos(numero){
-    
-    let divisor = 2
-
-    while(numero%divisor != 0){
-        divisor++
-            if(numero == divisor){
-            alert('O número ' + numero + ' é primo')
-            } else {
-            alert ('O número ' + numero + ' não é primo')
-            }
-    }
-
-}
-
-function proximosPrimos(){
-
-    let inicio = numero 
-    let fim = numero + 100
-
-    for (let i = inicio; i<= fim; i++){
-        let divisores = 0
-        for (let aux = 1; aux< i; aux++){
-            divisores++
-            if (divisores == 2){
-                alert('O próximo número primo é ' + i)
-            }
-        }
-    }
-}
-
-insereNumero()
-*/
-
 let mesesAno = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
-
+/*let mesesCom31 = [mesesAno[0], mesesAno[2], mesesAno[4], mesesAno[6], mesesAno[7], mesesAno[9], mesesAno[11]]
+let mesesCom30 = [mesesAno[3], mesesAno[5], mesesAno[8], mesesAno[10]]
+let mesCom28 = mesesAno[1]
+*/
 function recebeMesNumero(){
     let numeroMes = prompt('Digite um número entre 1 e 12, correspondente ao mês que deseja consultar')  
-    return numeroMes 
+    return numeroMes
 }
 
 let armazena = recebeMesNumero()
 
-function nomeMes(armazena){
-    switch (armazena){
-        case '1':
-            alert('O mês escolhido foi ' + mesesAno[0])
-            break;
-        case '2':
-            alert('O mês escolhido foi ' + mesesAno[1])
-            break; 
-        case '3':
-            alert('O mês escolhido foi ' + mesesAno[2])
-            break;
-        case '4': 
-            alert('O mês escolhido foi ' + mesesAno[3])
-            break;
-        case '5':
-            alert('O mês escolhido foi ' + mesesAno[4])
-            break;
-        case '6': 
-            alert('O mês escolhido foi ' + mesesAno[5])
-            break;
-        case'7':
-            alert('O mês escolhido foi ' + mesesAno[6])
-            break;
-        case '8': 
-            alert('O mês escolhido foi ' + mesesAno[7])
-            break;
-        case '9':
-            alert('O mês escolhido foi ' + mesesAno[8])
-            break;
-        case '10': 
-            alert('O mês escolhido foi ' + mesesAno[9])
-            break;
-        case '11':
-            alert('O mês escolhido foi ' + mesesAno[10])
-            break;
-        case '12': 
-            alert('O mês escolhido foi ' + mesesAno[11])
-            break;
-        default:
-            alert('Número inválido!\nRecarregue a pagina para tentar novamente')
-            break;
+function nomeMes(){
+
+    if(armazena == 1){
+        alert('O mês escolhido foi ' + mesesAno[0])       
+    } else if (armazena == 2){
+        alert('O mês escolhido foi ' + mesesAno[1]) 
+    } else if (armazena == 3){
+        alert('O mês escolhido foi ' + mesesAno[2]) 
+    } else if (armazena == 4){
+        alert('O mês escolhido foi ' + mesesAno[3]) 
+    } else if (armazena == 5){
+        alert('O mês escolhido foi ' + mesesAno[4]) 
+    } else if (armazena == 6){
+        alert('O mês escolhido foi ' + mesesAno[5]) 
+    } else if (armazena == 7){
+        alert('O mês escolhido foi ' + mesesAno[6]) 
+    } else if (armazena == 8){
+        alert('O mês escolhido foi ' + mesesAno[7]) 
+    } else if (armazena == 9){
+        alert('O mês escolhido foi ' + mesesAno[8]) 
+    } else if (armazena == 10){
+        alert('O mês escolhido foi ' + mesesAno[9]) 
+    } else if (armazena == 11){
+        alert('O mês escolhido foi ' + mesesAno[10]) 
+    } else if (armazena == 12){
+        alert('O mês escolhido foi ' + mesesAno[11]) 
+    } else {
+        alert ('Número inválido\nRecarregue a página')
+    }
+}
+
+function quantosDiasMeses(){
+    if(armazena == 1 || armazena == 3 || armazena == 5 || armazena == 7 || armazena == 8 || armazena == 10 || armazena == 12){
+        alert('O mês escolhido tem 31 dias')
+    } else if (armazena == 4 || armazena == 6 || armazena == 9) {
+        alert('O mês escolhido tem 30 dias')
+    } else {
+        alert('O mês escolhido tem 28 dias')
     }
 }
 
 
 recebeMesNumero()
 nomeMes()
+quantosDiasMeses()
