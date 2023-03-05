4388624eb7c37e8914d038fc2ffa66a16d916c8e
zirtaebea <zirtaeb.dev@gmail.com>
2023-03-05 17:07:20 -0300

    atividade

--- 0_Projeto_integrador.js
+++ 0_Projeto_integrador.js
@@ -1,168 +1,161 @@
-/*
-ideia: selecionar bairro e depois: 
-1- chamar função para escolher
-se quer café da manhã almoço ou jantar 
-2- colocar dentro de cada classe um restaurante atribuido
-as propriedades do construtor
-3- fazer uma ultima função de retornar ao menu principal
-*/
-
-//ATENCAO O PROJETO ATUAL CONTEM APLICACOES DAS AULAS 4 E 5 (FUNCOES E OBJETOS)
+//armazenando os objetos em arrays
+const cafe = [{id: 1, nome: 'Bolo da Luz', preco: 1, avaliacaoUsuario: 4 },
+              {id: 1, nome: 'Casa de Noca', preco: 3, avaliacaoUsuario: 4 },
+              {id: 1, nome: 'Santo Pão Gourmet', preco: 3, avaliacaoUsuario: 5 },
+              {id: 2, nome: 'Padoca do Carmo', preco: 1, avaliacaoUsuario: 3 },
+              {id: 2, nome: 'Pousada des Artes', preco: 2, avaliacaoUsuario: 5 },
+              {id: 2, nome: 'Cafélier', preco: 3, avaliacaoUsuario: 4 }]
 
 
-//iniciar
-let escolhaBairro = prompt('Escolha o bairro que você deseja:\n1 - Pituba\n2 - Centro histórico\nDigite apenas o número correspondente')
-
-//arrays pituba
-let listaCafePituba = ['Cantina Cosa Notra', ' Casa de Noca', ' Santo Pão Gourmet']
-let listaAlmocoPituba = ['Bella Gourmet Bistrot', ' Arabesque Empório Árabe', ' Restaurante Martim Pescador'] 
-let listaJantarPituba = ['La Pasta Gialla', ' Cantina Buoni Amici', ' Origem ']
-
-
-//arrays centro
-let listaCafeCentro = ['Padoca do Carmo', ' Pousada des Artes', ' Cafélier']
-let listaAlmocoCentro = ['Restaurante Pelô Bistrô', ' Omí Restaurante', ' Restaurante Mistura Contorno']
-let listaJantarCentro = ['Bar Ulisses', ' Poró Restaurante & Bar', ' Zanzibar']
+const almoco = [{id: 1, nome: 'Bella Gourmet Bistrot', preco: 4, avaliacaoUsuario: 4},
+                {id: 1, nome: 'Arabesque Empório Árabe', preco: 2, avaliacaoUsuario: 3 },
+                {id: 1, nome: 'Restaurante Martim Pescador', preco: 3, avaliacaoUsuario: 4 },
+                {id: 2, nome: 'Restaurante Pelô Bistrô', preco: 3, avaliacaoUsuario: 3 },
+                {id: 2, nome: 'Omí Restaurante', preco: 4, avaliacaoUsuario: 4 },
+                {id: 2, nome: 'Restaurante Mistura Contorno', preco: 5, avaliacaoUsuario: 5}]
 
 
-//sendo id 1 - pituba e id 2 - centro
-const cafeDaManha = [{id: 1, restaurante: "Cantina Cosa Notra"},
-                     {id: 1, restaurante: "Casa de Noca"},
-                     {id: 1, restaurante: "Santo Pão Gourmet"},
-                     {id: 2, restaurante: "Padoca do Carmo"}, 
-                     {id: 2, restaurante:"Pousada das Artes"}, 
-                     {id: 2, restaurante: "Cafélier"}]
+const jantar = [{id: 1, nome: 'La Pasta Gialla', preco: 2, avaliacaoUsuario: 4 },
+                {id: 1, nome: 'Cantina Buoni Amici', preco: 2, avaliacaoUsuario: 3},
+                {id: 1, nome: 'Origem', preco: 5, avaliacaoUsuario: 5},
+                {id: 2, nome: 'Bar Ulisses', preco: 2, avaliacaoUsuario: 5},
+                {id: 2, nome: 'Poró Restaurante & Bar', preco: 3, avaliacaoUsuario: 4},
+                {id: 2, nome: 'Zanzibar', preco: 2, avaliacaoUsuario: 4 }]
 
-const almoco = [{id: 1, restaurante: "Bella Gourmet Bistrot"},
-                {id: 1, restaurante: "Arabesque Empório Árabe"},
-                {id: 1, restaurante: "Restaurante Martim Pescador"},
-                {id: 2, restaurante: "Restaurante Pelô Bistrô"}, 
-                {id: 2, restaurante: "Omí Restaurante"}, 
-                {id: 2, restaurante: "Restaurante Mistura Contorno"}]                    
-
-const jantar = [{id: 1, restaurante: "La Pasta Gialla"},
-                {id: 1, restaurante: "Cantina Buoni Amici"},
-                {id: 1, restaurante: "Origem"},
-                {id: 2, restaurante: "Bar Ulisses"}, 
-                {id: 2, restaurante:"Poró Restaurante & Bar"}, 
-                {id: 2, restaurante: "Zanzibar"}] 
-
-for (const restaurante of cafeDaManha){
-    console.log(restaurante.id);
-    console.log(restaurante.restaurante)
-}                    
-
-/*const produtos = [{ id: 1, produto: "Arroz" },
-                  { id: 2,  produto: "Macarrão" },
-                  { id: 3,  produto: "Pão" }];
-for (const produto of produtos) {
-    console.log(produto.id);
-    console.log(produto.produto);
-
-for (const produto of produtos) {
-    console.log(produto.id);
-    console.log(produto.produto);
-
-    --------------------------------------------------
-    class Produto {
-        constructor(nome, preco) {
-            this.nome = nome.toUpperCase();
-            this.preco = parseFloat(preco);
-            this.vendido = false;
-        }
-        somaICMS() {
-            this.preco = this.preco * 1.21;
-        }
-    }
-    //Declaramos um array de produtos para armazenar objetos
-    const produtos = [];
-    produtos.push(new Produto("arroz", "125"));
-    produtos.push(new Produto(“macarrão", "70"));
-    produtos.push(new Produto("pão", "50"));
-    //Iteramos o array com for...of para modificar todos
-    for (const produto of produtos)
-        produto.somaICMS();
-*/
-
-//objeto pituba com restaurantes e metodo
-class pituba{
-    constructor(restaurantesCafe, restaurantesAlmoco, restauranteJantar){
-        this.cafeManha = restaurantesCafe
-        this.almoco = restaurantesAlmoco
-        this.jantar = restauranteJantar
-    }
-   
-    escolherRefeicaoPituba(){ //METODO -- COMO CHAMAR? nomeDaConst.metodo()
-        let escolhaPituba = prompt('Escolha o tipo de refeição\n1 - Café da manhã\n2 - Almoço\n3 - Jantar\nDigite apenas o número correspondente')
-            if (escolhaPituba == 1){
-                alert('Estes são os restaurantes disponíveis:\n' + this.cafeManha)
-                menuNovamente()
-            } else if (escolhaPituba == 2) {
-                alert('Estes são os restaurantes disponíveis:\n' + this.almoco)
-                menuNovamente()
-            } else if (escolhaPituba == 3){
-                alert('Estes são os restaurantes disponíveis:\n' + this.jantar)
-                menuNovamente()
-            }
-
-    }
-}
-
-const restaurantesPituba = new pituba(listaCafePituba, listaAlmocoPituba, listaJantarPituba) //guardando os valores na const
-
-//objeto centro com restaurantes e metodo
-class centro{
-    constructor(resCafe, resAlmoco, resJantar){
-        this.cafe = resCafe
-        this.almoco = resAlmoco
-        this.jantar = resJantar
-    }
-
-    escolherRefeicaoCentro(){
-        let escolhaCentro = prompt('Escolha o tipo de refeição\n1 - Café da manhã\n2 - Almoço\n3 - Jantar\nDigite apenas o número correspondente')
-        if (escolhaCentro == 1){
-            alert('Estes são os restaurantes disponíveis:\n' + this.cafe)
-            menuNovamente()
-        } else if (escolhaCentro == 2) {
-            alert('Estes são os restaurantes disponíveis:\n' + this.almoco)
-            menuNovamente()
-        } else if (escolhaCentro == 3){
-            alert('Estes são os restaurantes disponíveis:\n' + this.jantar)
-            menuNovamente()
-        }
-
-    }
-}
-
-const restaurantesCentro = new centro(listaCafeCentro, listaAlmocoCentro, listaJantarCentro) //guardando os valores na const 
+const cafeAlmoco = cafe.concat(almoco)
+const geral = cafeAlmoco.concat(jantar)
 
 //criando funções
-function menuBairro(escolhaBairro){
-    if (escolhaBairro == 1){
-        alert('Por favor escolha o tipo de refeição')
-        restaurantesPituba.escolherRefeicaoPituba()
-    } else if (escolhaBairro == 2){
-        alert('Por favor escolha o tipo de refeição')
-        restaurantesCentro.escolherRefeicaoCentro()
+function objetosString(x, y){
+    let str = "";
+    for (let i = 0; i < geral.length; i++) {
+        let numeroAcompanha = i + 1
+        str += numeroAcompanha + ' - ' + geral[i].nome + "\n";
+    }
+
+    alert(x + y + ' são:\n\n' + str)
+}
+
+function opcaoMenu(op){
+    if (op == 1){
+        porBairro()
+    } else if (op == 2){
+        porPreco()
+    } else if (op == 3){
+        porAvaliacao()
+    } else if (op == 4){
+        mostraTodos()
     } else {
-        alert('Valor inválido, tente novamente')
+        alert('Opção inválida!\nTente novamente')
         menuNovamente()
     }
+    
+}
+
+function porBairro(){
+    let bairro = prompt('Escolha o bairro que você deseja:\n1 - Pituba\n2 - Centro histórico\nDigite apenas o número correspondente') 
+    if (bairro == 1){
+        const pitubaCafe = cafe.filter(idCafe => idCafe.id == 1)
+        const pitubaAlmoco = almoco.filter(idAlmoco => idAlmoco.id == 1)
+        const pitubaJantar = jantar.filter(idJantar => idJantar.id == 1)
+        
+        alert('Esses são os restaurantes disponíveis no bairro da Pituba')
+
+        alert('Café da manhã:\n' + pitubaCafe[0].nome + '\n' + pitubaCafe[1].nome + '\n' + pitubaCafe[2].nome)
+        alert('Almoço:\n' + pitubaAlmoco[0].nome + '\n' + pitubaAlmoco[1].nome + '\n' + pitubaAlmoco[2].nome)
+        alert('Jantar:\n' + pitubaJantar[0].nome + '\n' + pitubaJantar[1].nome + '\n' + pitubaJantar[2].nome)
+
+        menuNovamente()
+
+    } else if (bairro == 2){
+        
+        const centroCafe = cafe.filter(idCafe => idCafe.id == 2)
+        const centroAlmoco = almoco.filter(idAlmoco => idAlmoco.id == 2)
+        const centroJantar = jantar.filter(idJantar => idJantar.id == 2)
+        
+        alert('Esses são os restaurantes disponíveis no bairro do Centro')
+
+        alert('Café da manhã:\n' + centroCafe[0].nome + '\n' + centroCafe[1].nome + '\n' + centroCafe[2].nome)
+        alert('Almoço:\n' + centroAlmoco[0].nome + '\n' + centroAlmoco[1].nome + '\n' + centroAlmoco[2].nome)
+        alert('Jantar:\n' + centroJantar[0].nome + '\n' + centroJantar[1].nome + '\n' + centroJantar[2].nome)
+
+        menuNovamente()
+
+    } else {
+        alert('Opção inválida!\nTente novamente')
+        menuNovamente()
+    }
+}
+
+function porPreco(){
+    let ordenar = prompt('Como você deseja ordenar?\n 1 - Ordem Crescente\n 2 - Ordem Decrescente')
+    if(ordenar == 1){
+
+        geral.sort(function(a,b) {
+            if(a.preco > b.preco) return 1;
+            if(a.preco < b.preco) return -1;
+            return 0;      
+        })
+
+        let textoInicial = 'Os restaurantes ordenados '
+        let ordemCrescente = 'do menor para o maior preço'
+
+        objetosString(textoInicial, ordemCrescente)
+        menuNovamente()
+
+    } else if (ordenar == 2){
+        
+        geral.sort(function(a,b) {
+            if(a.preco > b.preco) return -1;
+            if(a.preco < b.preco) return 1;
+            return 0;  
+        })
+        
+        let textoInicial = 'Os restaurantes ordenados '
+        let ordemDecrescente = 'maior para o menor preço'
+
+        objetosString(textoInicial, ordemDecrescente)
+        menuNovamente()
+
+    } else {
+        alert('Opção inválida!\nTente novamente')
+        menuNovamente()
+    }
+}
+
+function porAvaliacao(){
+    geral.sort(function (a, b){
+        if(a.avaliacaoUsuario > b.avaliacaoUsuario) return -1;
+        if(a.avaliacaoUsuario < b.avaliacaoUsuario) return 1;
+        return 0; 
+    })
+    let textoInicial = 'Os restaurantes ordenados '
+    let ordemAvaliacao = 'da melhor para a pior avaliação de usuário'
+    objetosString(textoInicial, ordemAvaliacao)
+
+    menuNovamente()
 
 }
 
+function mostraTodos(){
+    let textoInicial = 'Todos os restaurantes disponíveis são:'
+    let quebraLinha = '\n'
+
+    objetosString(textoInicial, quebraLinha)
+    menuNovamente()
+}
 
 function menuNovamente(){
     let perguntaContinuar = prompt('Deseja tentar novamente?\n1 - Sim\n2 - Não\nDigite apenas o número correspondente')
     if(perguntaContinuar == 1){
-        let escolhaBairro = prompt('Escolha o bairro que você deseja:\n1 - Pituba\n2 - Centro histórico\nDigite apenas o número correspondente')
-        menuBairro(escolhaBairro)
+        let iniciarNovamente = prompt('Bem vindo ao buscador de restaurantes em Salvador!\nComo deseja buscar?\n 1 - Por bairro\n 2 - Por preço\n 3 - Por avaliação\n 4 - Mostrar todos')
+        opcaoMenu(iniciarNovamente)
     } else if (perguntaContinuar == 2){
         alert('Volte sempre!')
     } 
 
 }
 
-
-//chamando a funcao
-menuBairro(escolhaBairro)
+//inicializando e chamando função
+let menuIniciar = prompt('Bem vindo ao buscador de restaurantes em Salvador!\nComo deseja buscar?\n 1 - Por bairro\n 2 - Por preço\n 3 - Por avaliação\n 4 - Mostrar todos')
+opcaoMenu(menuIniciar)
