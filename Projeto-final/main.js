f6f10dcc4e52936056318e44bca7b64ef3d45aa9
zirtaebea <zirtaeb.dev@gmail.com>
2023-04-10 20:35:30 -0300

    Live Server

--- Projeto-final/main.js
+++ Projeto-final/main.js
@@ -1,204 +1,4 @@
-//armazenando os objetos em arrays
-const geral = [
-  {
-    id: 1,
-    imagem:
-      "https://pelourinhodiaenoite.salvador.ba.gov.br/wp-content/uploads/2018/11/IMG_0011.jpg",
-    bairro: "Pituba",
-    nome: "Bolo da Luz",
-    ref: "cafe-da-manha",
-    preco: "$",
-    avaliacaoUsuario: 4,
-    favorito: " ",
-  },
-  {
-    id: 1,
-    imagem:
-      "https://pelourinhodiaenoite.salvador.ba.gov.br/wp-content/uploads/2018/11/IMG_0011.jpg",
-    bairro: "Pituba",
-    nome: "Casa de Noca",
-    ref: "cafe-da-manha",
-    preco: "$$$",
-    avaliacaoUsuario: 4,
-    favorito: " ",
-  },
-  {
-    id: 1,
-    imagem:
-      "https://pelourinhodiaenoite.salvador.ba.gov.br/wp-content/uploads/2018/11/IMG_0011.jpg",
-    bairro: "Pituba",
-    nome: "Santo Pão Gourmet",
-    ref: "cafe-da-manha",
-    preco: "$$$",
-    avaliacaoUsuario: 5,
-    favorito: " ",
-  },
-  {
-    id: 2,
-    imagem:
-      "https://pelourinhodiaenoite.salvador.ba.gov.br/wp-content/uploads/2018/11/IMG_0011.jpg",
-    bairro: "Centro",
-    nome: "Padoca do Carmo",
-    ref: "cafe-da-manha",
-    preco: "$",
-    avaliacaoUsuario: 3,
-    favorito: " ",
-  },
-  {
-    id: 2,
-    imagem:
-      "https://pelourinhodiaenoite.salvador.ba.gov.br/wp-content/uploads/2018/11/IMG_0011.jpg",
-    bairro: "Centro",
-    nome: "Pousada des Artes",
-    ref: "cafe-da-manha",
-    preco: "$$",
-    avaliacaoUsuario: 5,
-    favorito: " ",
-  },
-  {
-    id: 2,
-    imagem:
-      "https://pelourinhodiaenoite.salvador.ba.gov.br/wp-content/uploads/2018/11/IMG_0011.jpg",
-    bairro: "Centro",
-    nome: "Cafélier",
-    ref: "cafe-da-manha",
-    preco: "$$$",
-    avaliacaoUsuario: 4,
-    favorito: " ",
-  },
-  {
-    id: 1,
-    imagem:
-      "https://pelourinhodiaenoite.salvador.ba.gov.br/wp-content/uploads/2018/11/IMG_0011.jpg",
-    bairro: "Pituba",
-    nome: "Bella Gourmet Bistrot",
-    ref: "almoco",
-    preco: "$$$$",
-    avaliacaoUsuario: 4,
-    favorito: " ",
-  },
-  {
-    id: 1,
-    imagem:
-      "https://pelourinhodiaenoite.salvador.ba.gov.br/wp-content/uploads/2018/11/IMG_0011.jpg",
-    bairro: "Pituba",
-    nome: "Arabesque Empório Árabe",
-    ref: "almoco",
-    preco: "$$",
-    avaliacaoUsuario: 3,
-    favorito: " ",
-  },
-  {
-    id: 1,
-    imagem:
-      "https://pelourinhodiaenoite.salvador.ba.gov.br/wp-content/uploads/2018/11/IMG_0011.jpg",
-    bairro: "Pituba",
-    nome: "Restaurante Martim Pescador",
-    ref: "almoco",
-    preco: "$$$",
-    avaliacaoUsuario: 4,
-    favorito: " ",
-  },
-  {
-    id: 2,
-    imagem:
-      "https://pelourinhodiaenoite.salvador.ba.gov.br/wp-content/uploads/2018/11/IMG_0011.jpg",
-    bairro: "Centro",
-    nome: "Restaurante Pelô Bistrô",
-    ref: "almoco",
-    preco: "$$$",
-    avaliacaoUsuario: 3,
-    favorito: " ",
-  },
-  {
-    id: 2,
-    imagem:
-      "https://pelourinhodiaenoite.salvador.ba.gov.br/wp-content/uploads/2018/11/IMG_0011.jpg",
-    bairro: "Centro",
-    nome: "Omí Restaurante",
-    ref: "almoco",
-    preco: "$$$",
-    avaliacaoUsuario: 4,
-    favorito: " ",
-  },
-  {
-    id: 2,
-    imagem:
-      "https://pelourinhodiaenoite.salvador.ba.gov.br/wp-content/uploads/2018/11/IMG_0011.jpg",
-    bairro: "Centro",
-    nome: "Restaurante Mistura Contorno",
-    ref: "almoco",
-    preco: "$$$$$",
-    avaliacaoUsuario: 5,
-    favorito: " ",
-  },
-  {
-    id: 1,
-    imagem:
-      "https://pelourinhodiaenoite.salvador.ba.gov.br/wp-content/uploads/2018/11/IMG_0011.jpg",
-    bairro: "Pituba",
-    nome: "La Pasta Gialla",
-    ref: "jantar",
-    preco: "$$",
-    avaliacaoUsuario: 4,
-    favorito: " ",
-  },
-  {
-    id: 1,
-    imagem:
-      "https://pelourinhodiaenoite.salvador.ba.gov.br/wp-content/uploads/2018/11/IMG_0011.jpg",
-    bairro: "Pituba",
-    nome: "Cantina Buoni Amici",
-    ref: "jantar",
-    preco: "$$",
-    avaliacaoUsuario: 3,
-    favorito: " ",
-  },
-  {
-    id: 1,
-    imagem:
-      "https://pelourinhodiaenoite.salvador.ba.gov.br/wp-content/uploads/2018/11/IMG_0011.jpg",
-    bairro: "Pituba",
-    nome: "Origem",
-    ref: "jantar",
-    preco: "$$$$$",
-    avaliacaoUsuario: 5,
-    favorito: " ",
-  },
-  {
-    id: 2,
-    imagem:
-      "https://pelourinhodiaenoite.salvador.ba.gov.br/wp-content/uploads/2018/11/IMG_0011.jpg",
-    bairro: "Centro",
-    nome: "Bar Ulisses",
-    ref: "jantar",
-    preco: "$$",
-    avaliacaoUsuario: 5,
-    favorito: " ",
-  },
-  {
-    id: 2,
-    imagem:
-      "https://pelourinhodiaenoite.salvador.ba.gov.br/wp-content/uploads/2018/11/IMG_0011.jpg",
-    bairro: "Centro",
-    nome: "Poró Restaurante & Bar",
-    ref: "jantar",
-    preco: "$$$",
-    avaliacaoUsuario: 4,
-    favorito: " ",
-  },
-  {
-    id: 2,
-    imagem:
-      "https://pelourinhodiaenoite.salvador.ba.gov.br/wp-content/uploads/2018/11/IMG_0011.jpg",
-    bairro: "Centro",
-    nome: "Zanzibar",
-    ref: "jantar",
-    preco: "$$",
-    avaliacaoUsuario: 4,
-    favorito: " ",
-  },
-];
+//acessar arquivo com extensão 'live server' do VSCODE
 
 //acessando html
 const novoEvento = document.getElementById("novoEvent");
@@ -208,22 +8,35 @@ const filtrosgeral = document.querySelectorAll(".nomeref");
 //filtros
 let filtro = "";
 
-filtrosgeral.forEach((element) => {
-  element.onclick = (e) => {
-    console.log(e);
-    //condicional acesso a um obj
-    filtro = e?.target?.id;
-    console.log(filtro);
-    mostraRestaurante();
-  };
-});
+//loading div loading -> class show/hidden
+
+//armazenando os objetos em arrays
+const geral = fetch("restaurantes.json")
+  .then((resp) => resp.json())
+  .then((listaRest) => {
+    console.log("teste");
+    filtrosgeral.forEach((element) => {
+      element.onclick = (e) => {
+        console.log(e);
+        //condicional acesso a um obj
+        filtro = e?.target?.id;
+        console.log(filtro);
+        mostraRestaurante(listaRest);
+      };
+    });
+    novoEvento.addEventListener("submit", (e) =>
+      salvarNovoEvento(e, listaRest)
+    );
+  });
 
 //consultando o input
-function consultarBairro() {
+function consultarBairro(listaRest) {
   const nomebairro = inputBairro.value.toUpperCase();
   if (nomebairro != "") {
     //operador ternario
-    return geral.some((local) => local.bairro.toLocaleUpperCase() == nomebairro)
+    return listaRest.some(
+      (local) => local.bairro.toLocaleUpperCase() == nomebairro
+    )
       ? nomebairro
       : alert("Bairro não cadastrado!");
   } else {
@@ -233,42 +46,46 @@ function consultarBairro() {
 }
 
 //evento de evitar redirecionar para outra pagina
-function salvarNovoEvento(e) {
+function salvarNovoEvento(e, listaRest) {
   e.preventDefault();
-  let bairro = inputBairro.value;
-  mostraRestaurante(bairro);
+  //let bairro = inputBairro.value;
+  mostraRestaurante(listaRest);
 }
 
 //escrever lista de restaurantes
-function mostraRestaurante() {
+function mostraRestaurante(listaRest) {
   const restaurante = document.getElementById("card-restaurantes");
   restaurante.innerHTML = "";
   let todos = [];
-  const verificaBairro = consultarBairro();
+  const verificaBairro = consultarBairro(listaRest);
+
   if (filtro != "") {
     const filtraTodos = "todos";
+
     //operador ternario
     filtro != filtraTodos
-      ? (todos = geral.filter(
+      ? (todos = listaRest.filter(
           (local) =>
             local.bairro.toUpperCase() == verificaBairro && filtro == local.ref
         ))
-      : (todos = geral.filter(
+      : (todos = listaRest.filter(
           (local) => local.bairro.toUpperCase() == verificaBairro
         ));
+
     //aba filtrados
     if (filtro == "favoritados") {
       let meuStorage = localStorage.restFavoritos;
       for (let i = 0; i < meuStorage.length; i++) {
-        todos = geral.filter((local) => local.favorito.includes("sim"));
+        todos = listaRest.filter((local) => local.favorito.includes("sim"));
         todos == meuStorage[i] ? meuStorage[i] : false;
       }
     }
   } else {
-    todos = geral.filter(
+    todos = listaRest.filter(
       (local) => local.bairro.toUpperCase() == verificaBairro
     );
   }
+
   for (let rest of todos) {
     let li = document.createElement("li");
     li.setAttribute("id", rest.nome);
@@ -279,25 +96,33 @@ function mostraRestaurante() {
                         ${rest.preco}
                         <br>
                         ${rest.avaliacaoUsuario}</p>
-                        <button onclick="addItem('${rest.nome}')" type="button" class="favorito">Favoritar</buttton>`;
+                        <button id="${rest.nome}" type="button" class="favorito">Favoritar</buttton>`;
     restaurante.appendChild(li);
+  }
+  const classFav = document.getElementsByClassName("favorito");
+  console.log(classFav);
+
+  for (let botao of classFav) {
+    botao.onclick = (e) => {
+      addItem(e.target.id, listaRest);
+    };
   }
 }
 
 //botao favoritar restaurante p/ usar o storage
 let arr = [];
-function addItem(element) {
+function addItem(element, listaRest) {
   console.log(element);
   localStorage.setItem("restFavoritos", arr);
 
   //mudando propriedade favoritos p/ 'sim'
-  for (let x of geral) {
+  for (let x of listaRest) {
     if (element == x.nome) {
       //pegando o id do elemento clicado
       let listinha = document.getElementById(element);
       console.log(listinha);
 
-      //mudando propriedade 'favorito' no array de obj geral
+      //mudando propriedade 'favorito' no array de obj listaRest
       x.favorito = "sim";
       console.log(x);
 
@@ -317,5 +142,3 @@ function addItem(element) {
     timer: 1500,
   });
 }
-
-novoEvento.addEventListener("submit", salvarNovoEvento);
